c6c4d791b56cd54e2a7bc96aff1fd330678041ab
Filipe Gonçalves <filipeg@ua.pt>
2024-01-16 15:39:41 +0000

    Add files via upload

--- Parte 2/TP3.m
+++ Parte 2/TP3.m
@@ -52,7 +52,6 @@ input("Press <ENTER> to CONTINUE")
 cam = webcam(wbcm);
 
 image = snapshot(cam);
-imshow(image);
 [im, newOrigin] = undistortImage(image, cameraParams, 'OutputView', 'full');
 [imagePoints, boardSize] = detectCheckerboardPoints(im);
 [R, t] = extrinsics(imagePoints, cameraParams.WorldPoints, cameraParams);
@@ -61,8 +60,8 @@ imshow(image);
 [H,S,V] = rgb2hsv(im);
 
 % mask the objects/plane by color
-greenObjectsMask = (H >= 0.2 & H <= 0.5 & S >= 0.2 & S <= 0.8 & V >= 0.2);
-redObjectsMask = (H <= 0.2 & S >= 0.8 & V >= 0.8);
+greenObjectsMask = (H > 0.2 & H < 0.5 & S > 0.4 & S < 1.0 & V > 0.3);
+redObjectsMask   = (H > 0.0 & H < 0.12 & S > 0.4 & S < 1.0 & V > 0.1);
 
 % Label connected components in the binary mask
 labeledRedImage = bwlabel(redObjectsMask);
@@ -78,54 +77,61 @@ num_green_objects = 1;
 Points = [];
 End_Object = [];
 
-% Get Red Plane
-[~, ind] = max([Redstats.Area]);
-Red_Plane = Redstats(ind).Centroid;
-Redstats(ind) = [];
-
-% Get green plane
-[~, ind] = max([Greenstats.Area]);
-Green_Plane = Greenstats(ind).Centroid;
-Greenstats(ind) = [];
+subplot(1,2,1);
+imshow(image)
+hold on;
 
 % Get red objects
-for i=1:num_red_objects
-    [~, ind] = max([Redstats.Area]);
-    Points = [Points; Redstats(ind).Centroid];
+[~, ind] = maxk([Redstats.Area], num_red_objects+1);
+Red_Plane = Redstats(ind(1)).Centroid;
+plot(Red_Plane(1), Red_Plane(2), "r*")
+for i=2:size(ind,2)
+    P = Redstats(ind(i)).Centroid;
+    Points = [Points; P];
     End_Object = [End_Object; Red_Plane];
-    Redstats(ind) = [];
+    plot(P(1), P(2), "r*")
 end
+
+subplot(1,2,2);
+imshow(image)
+hold on;
+
 % Get green objects
-for i=1:num_green_objects
-    [~, ind] = max([Greenstats.Area]);
-    Points = [Points; Greenstats(ind).Centroid];
+[~, ind] = maxk([Greenstats.Area], num_green_objects+1);
+Green_Plane = Greenstats(ind(1)).Centroid;  
+plot(Green_Plane(1), Green_Plane(2), "r*")
+for i=2:size(ind,2)
+    P = Greenstats(ind(i)).Centroid;
+    Points = [Points; P];
     End_Object = [End_Object; Green_Plane];
-    Greenstats(ind) = [];
+    plot(P(1), P(2), "r*")
 end
 
 %%% Move the robot
 % Move to each object and place it in the correct plane 
 for i=1:size(Points, 1)
+
     input("Press <ENTER> to CONTINUE")
 
     % Get actual position
     Pos = Get_Cart_Abs(robCOMM);
 
     % R T N -> Transform of N in R
-    T = [ 0 1 0  325
-          1 0 0 -440
-          0 0 0 -160
+    T = [ 0 1 0  327
+          1 0 0 -459
+          0 0 0 -206
           0 0 0  0   ];
 
     % Convert object camera point to real world point
     object = [pointsToWorld(cameraParams, R, t, [Points(i,1), Points(i,2)]) 1 1];
     object_robo = T * object';
     P = [object_robo(1)-Pos(1,1), object_robo(2)-Pos(1,2), 164-Pos(1,3)];
-
-    % Robot goes down to pick up object
+    
+    % Robot goes to the object and down to pick up the object
     Mov_Cart_Inc(robCOMM,P(1),P(2),P(3),0,0,1,1)
     pause(0.5);
-    Mov_Cart_Inc(robCOMM,0,0,-331,0,0,1,1)
+    Mov_Cart_Inc(robCOMM,0,0,-333,0,0,1,1)
+    pause(0.5);
 
     % Update robot position
     object_robo = [object_robo(1), object_robo(2), 164-331];
@@ -135,6 +141,7 @@ for i=1:size(Points, 1)
 
     % Robot goes up
     Mov_Cart_Inc(robCOMM,0,0,160,0,0,1,1)
+    pause(0.5);
 
     % Update robot position
     object_robo = [object_robo(1), object_robo(2), object_robo(3)+160];
@@ -146,19 +153,32 @@ for i=1:size(Points, 1)
 
     % Robot goes to the plane
     Mov_Cart_Inc(robCOMM,P(1),P(2),0,0,0,1,1)
+    pause(0.5);
 
     % Update robot position
     object_robo = [Plane(1), Plane(2), object_robo(3)];
 
     % Robot goes down
-    Mov_Cart_Inc(robCOMM,0,0,-150,0,0,1,1)
+    Mov_Cart_Inc(robCOMM,0,0,-160+60*(i/2),0,0,1,1)
+    pause(0.5);
 
     % Update robot position
-    object_robo = [object_robo(1), object_robo(2), object_robo(3)-150];
+    object_robo = [object_robo(1), object_robo(2), object_robo(3)-160];
 
     % Turn off vacuum
     Gripper_Open(robCOMM, 2);
 end
 
 %%% End program
+input("Press <ENTER> to CONTINUE")
+
+% Get actual position
+Pos = Get_Cart_Abs(robCOMM);
+
+% Move robot to initial position
+P = [400, 0, 220];
+newP = [P(1)-Pos(1,1), P(2)-Pos(1,2), P(3)-Pos(1,3)];
+Mov_Cart_Inc(robCOMM,newP(1),newP(2),newP(3),0,0,1,1)
+
+% End
 fprintf("DONE\n");
